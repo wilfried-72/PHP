16cb096f03d1799c2948f6bd2063dc5a89fdb29d
wilfried-72 <wilfried@mail.cii-telecom.fr>
2022-10-13 16:45:56 +0200

    rename dossier des tutos de ce repo

---
+++ site-php-poo-mysql/env.php
@@ -0,0 +1,7 @@
+<?php
+   $GLOBALS['APP_ENV'] = "dev";
+   $GLOBALS['DATABASE_NAME'] = "crud_tutorial";
+   $GLOBALS['DATABASE_HOST'] = "localhost";
+   $GLOBALS['DATABASE_USER'] = "wil";
+   $GLOBALS['DATABASE_PASSWORD'] = "Cii$22";
+?>
